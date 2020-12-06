d615f68c9c012a6cadab06575431b7bf2828fe3c
Mike Crockett <github@mmcrockett.com>
2020-12-06 13:12:10 -0600

    day1

--- app/models/day1.rb
+++ app/models/day1.rb
@@ -1,34 +1,28 @@
 class Day1
-  def initialize
-    @lines = File.open(Rails.root.join('test', 'fixtures', 'files', 'day1.txt')).each_line.map(&:chomp)
+  #1769 too low
+  include FileName
+
+  def initialize(file: nil, file_ext: nil)
+    @data = load_data(file_name(file: file, file_ext: file_ext))
+  end
+
+  def load_data(file)
+    File.open(file).each_line.map(&:chomp)
   end
 
   def part1
-    @lines.size.times do | i|
-      (i + 1..@lines.size).each do |j|
-        a = @lines[i].to_i
-        b = @lines[j].to_i
-
-        sum = a + b
-
-        return a * b if 2020 == sum
-      end
-    end
+    @data.map {|l| l.chars.map {|v| '(' == v ? 1 : -1 }.sum }
   end
 
   def part2
-    @lines.size.times do | i|
-      (i + 1..@lines.size).each do |j|
-        (j + 1..@lines.size).each do |k|
-          a = @lines[i].to_i
-          b = @lines[j].to_i
-          c = @lines[k].to_i
+    floor = 0
 
-          sum = a + b + c
+    @data.first.chars.each_with_index do |v, i|
+      floor += '(' == v ? 1 : -1
 
-          return a * b * c if 2020 == sum
-        end
-      end
+      return (i + 1) if floor < 0
     end
+
+    floor
   end
 end
