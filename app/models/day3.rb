c5ab5ff617c53e8cc2338303c070d86dec940400
Mike Crockett <github@mmcrockett.com>
2020-12-06 19:48:19 -0600

    day3

--- app/models/day3.rb
+++ app/models/day3.rb
@@ -1,54 +1,68 @@
 class Day3
   include FileName
-  include TwoDimensions
-
-  TREE = '#'.freeze
 
   def initialize(file: nil, file_ext: nil)
-    @data = load_data(file_name(file: file, file_ext: file_ext))
+    @data     = load_data(file_name(file: file, file_ext: file_ext))
+  end
+
+  def load_data(file)
+    File.open(file).each_line.map(&:chomp)[0]
+  end
+
+  def add_to(l, i, amount)
+    l[i] += amount
   end
 
   def part1
-    t = 0
-    y = 0
-    x = 0
+    location = [0,0]
+    presents = { location.join('_') => 1 }
 
-    while
-      x += 3
-      y += 1
+    @data.chars.each do |direction|
+      case direction
+      when '>'
+        location[0] += 1
+      when '<'
+        location[0] -= 1
+      when '^'
+        location[1] += 1
+      when 'v'
+        location[1] -= 1
+      else
+        raise "!ERROR: #{direction}"
+      end
 
-      break if y >= height
-
-      x = (x % width) if x >= width
-
-      t += 1 if Day3::TREE == @data[y][x]
+      presents[location.join('_')] ||= 0
+      presents[location.join('_')]  += 1
     end
 
-    t
+    presents
   end
 
   def part2
-    t = []
+    slocation = [0,0]
+    rlocation = [0,0]
+    presents = { slocation.join('_') => 2 }
 
-    [[1,1],[3,1],[5,1],[7,1],[1,2]].each do |x_n, y_n|
-      y  = 0
-      x  = 0
-      tn = 0
+    @data.chars.each_with_index do |direction, i|
+      loc = i.even? ? slocation : rlocation
 
-      while
-        x += x_n
-        y += y_n
-
-        break if y >= height
-
-        x = (x % width) if x >= width
-
-        tn += 1 if Day3::TREE == @data[y][x]
+      case direction
+      when '>'
+        add_to(loc, 0, 1)
+      when '<'
+        add_to(loc, 0, -1)
+      when '^'
+        add_to(loc, 1, 1)
+      when 'v'
+        add_to(loc, 1, -1)
+      else
+        raise "!ERROR: #{direction}"
       end
 
-      t << tn
+      presents[loc.join('_')] ||= 0
+      presents[loc.join('_')]  += 1
     end
 
-    t.reduce(&:*)
+    presents
   end
 end
