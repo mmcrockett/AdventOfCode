88dbada7c8d18e8b4af47383f510e82440c00d3e
Mike Crockett <github@mmcrockett.com>
2020-12-08 21:34:03 -0600

    day5

--- app/models/day5.rb
+++ app/models/day5.rb
@@ -1,32 +1,113 @@
 class Day5
   include FileName
 
-  ROW_MAPPING = Array.new(128) {|t| format('%<v>07d', v: t.to_s(2)).gsub('0', 'F').gsub('1', 'B') }
-  COL_MAPPING = Array.new(8) {|t| format('%<v>03d', v: t.to_s(2)).gsub('0', 'L').gsub('1', 'R') }
+  ALWAYS_NAUGHTY = %w[ab cd pq xy].freeze
+  VOWELS         = %w[a e i o u].freeze
 
   def initialize(file: nil, file_ext: nil)
     @data = load_data(file_name(file: file, file_ext: file_ext))
-  end
-
-  def id(row: , col: )
-    row * 8 + col
   end
 
   def load_data(file)
     File.open(file).each_line.map(&:chomp)
   end
 
-  def part1
-    @data.map do |value|
-      id(row: ROW_MAPPING.index(value[0..6]), col: COL_MAPPING.index(value[7..9]))
+  def nice?(s)
+    has_double  = false
+    vowel_count = 0
+
+    return false if ALWAYS_NAUGHTY.any? {|z| s.include?(z) }
+
+    s.chars.each_with_index do |char, i|
+      has_double   = true if i > 0 && s[i - 1] == char
+      vowel_count += 1 if char.in?(VOWELS)
+
+      return true if has_double && vowel_count >= 3
     end
+
+    return false
   end
 
-  def part2
-    sorted_ids = self.part1.sort
+  def nice2?(s)
+    tuples = []
+    skip_repeat = false
+    set_repeat  = false
+    working_tuple = []
 
-    (0..sorted_ids.size - 2).each do |i|
-      return sorted_ids[i] + 1 if sorted_ids[i] + 2 == sorted_ids[i + 1]
+    s.chars.each_with_index do |char, i|
+      skip_repeat = true if i > 1 && s[i - 2] == char
+
+      if false == set_repeat
+        working_tuple << char
+
+        case working_tuple.size
+        when 2, 3
+          next unless 2 == working_tuple.uniq.size
+
+          while (working_tuple.size > 1)
+            tuples << working_tuple[0..1].join
+            working_tuple.shift
+          end
+        when 4
+          if 1 == working_tuple.uniq.size
+            set_repeat = true
+          else
+            tuples << working_tuple[0..1].join
+            tuples << working_tuple[2..-1].join
+            working_tuple.shift(3)
+          end
+        end
+
+        set_repeat = true if tuples.size != tuples.uniq.size
+      end
+
+      return true if skip_repeat && set_repeat
     end
+
+    case working_tuple.size
+    when 2
+      tuples << working_tuple[0..1].join
+    when 3
+      case working_tuple.uniq.size
+      when 2
+        tuples << working_tuple[0..1].join
+      when 3
+        tuples << working_tuple[0..1].join
+        tuples << working_tuple[1..-1].join
+      end
+    when 4
+      case working_tuple.uniq.size
+      when 1
+        set_repeat = true
+      when 2
+        tuples << working_tuple[0..1].join
+        tuples << working_tuple[2..-1].join
+      end
+    end
+
+    set_repeat = true if tuples.size != tuples.uniq.size
+
+    return set_repeat && skip_repeat
+  end
+
+  def part1
+    nice_count = 0
+
+    @data.each do |str|
+      nice_count += 1 if nice?(str)
+    end
+
+    nice_count
+  end
+
+  def part2(data = nil)
+    nice_count = 0
+    data ||= @data
+
+    data.each do |str|
+      nice_count += 1 if nice2?(str)
+    end
+
+    nice_count
   end
 end
