c58e47467296b235bd27feb6950747e8c6da5dbb
Mike Crockett <github@mmcrockett.com>
2020-12-11 10:28:24 -0600

    day11

--- app/models/concerns/two_dimensions.rb
+++ app/models/concerns/two_dimensions.rb
@@ -1,10 +1,6 @@
 module TwoDimensions
   extend ActiveSupport::Concern
 
-  def load_data(file)
-    File.open(file).each_line.map(&:chomp)
-  end
-
   def width
     @width ||= @data.first.size
   end
